370d7356deebbd6259ba94f73154f709b241ccb7
Nmerkel <noramerkel0@gmail.com>
2023-04-07 13:18:53 -0400

    Created addEdge method

---
+++ src/main/java/graphs/Main.java
@@ -0,0 +1,14 @@
+package graphs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+Graph myGraph=new Graph();
+myGraph.addVertex("A");
+myGraph.addVertex("B");
+myGraph.addEdge("A","B");
+myGraph.printGraph();
+
+    }
+}

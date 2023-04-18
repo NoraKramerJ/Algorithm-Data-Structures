994fb7d36def8029bd011ddd7fa7acfd2a4383cd
Nmerkel <noramerkel0@gmail.com>
2023-04-17 21:56:30 -0400

    lengthOfLongestSubstring

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,22 +3,21 @@ import java.util.stream.Collectors;
 
 public class main {
 
-        public static List<Integer> findDuplicates(int [] nums){
-            HashMap<Integer,Integer> myHashMap=new HashMap<>();
+    public static List<Integer> findDuplicates(int[] nums) {
+        HashMap<Integer, Integer> myHashMap = new HashMap<>();
 
-            List<Integer> list = new ArrayList<>();
-            for(int num:nums){
-                if(myHashMap.keySet().contains(num)){
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (myHashMap.keySet().contains(num)) {
 
-                    list.add(num);
-                }
-                else
-                    myHashMap.put(num, 0);
-
-            }
-            return list;
+                list.add(num);
+            } else
+                myHashMap.put(num, 0);
 
         }
+        return list;
+
+    }
 
 
     public static List<Integer> removeDuplicates(List<Integer> myList) {
@@ -26,9 +25,33 @@ public class main {
         return myList.stream().distinct().collect(Collectors.toList());
 
 
-
     }
-    public static boolean hasUniqueChars(String string){
+
+    public static int[] reverseString(int [] numbers){
+            int [] reverese=new int[numbers.length];
+            for(int i=0;i<numbers.length;i++){
+                reverese[numbers.length-1 -i]= numbers[i];
+
+
+        }
+        return reverese;
+    }
+
+    public static int lengthOfLongestSubstring(String s) { //bbbbb
+            int start = 0, end = 0, maxLength = 0;
+            Set<Character> charSet = new HashSet<>();
+            while (end < s.length()) {
+                if (!charSet.contains(s.charAt(end))) {
+                    charSet.add(s.charAt(end++));//b,1
+                    maxLength = Math.max(maxLength, end - start);//1
+                } else {
+                    charSet.remove(s.charAt(start++));//
+                }
+            }
+            return maxLength;//1
+        }
+
+        public static boolean hasUniqueChars(String string){
 
             Set<Character> mySet=new HashSet<>();
            char [] str= string.toCharArray();
@@ -40,10 +63,27 @@ public class main {
            return true;
     }
 
+
+
+   /* public static List<int[]> findPairs(int[] arr1, int[] arr2, int target){
+
+      Set<Integer> myset=new HashSet<>();
+
+    }*/
+      /*
+        Expected output:
+        [5, 2]
+        [3, 4]
+        [1, 6]
+    */
+
     public static void main(String[] args) {
         List<Integer> myList = List.of(1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5);
         List<Integer> newList = removeDuplicates(myList);
         System.out.println(newList);
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(Arrays.toString(reverseString(new int[] {
+            3,4,5})));
 
         /*
             EXPECTED OUTPUT:
